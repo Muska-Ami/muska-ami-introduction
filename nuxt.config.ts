6b04b96fafc23ac2f4ef551de42b893e74b0a3ee
夏沫花火zzz <xmcn@1l1.icu>
2023-10-20 23:59:53 +0800

    done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import components from 'unplugin-vue-components/vite'
-import autoImport from 'unplugin-auto-import/vite'
-import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
