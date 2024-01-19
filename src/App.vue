ff3295ef110f047c1a214c02c0e17fa4cbe22bca
夏沫花火zzz <xmcn@1l1.icu>
2024-01-20 03:05:09 +0800

    New

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+let year = new Date().getFullYear()
+</script>
+
+<template>
+  <main class="fame">
+    <el-card>
+      <template #header>
+        <h2>Hello, I'm 夏沫花火zzz🌙(Muska_Ami).</h2>
+      </template>
+      <div>
+        <p class="bds">
+          <h4>Languages that use</h4>
+          <img alt="Static Badge" src="https://img.shields.io/badge/%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87-66ccff?style=flat-square" />
+          <img alt="Static Badge" src="https://img.shields.io/badge/English-66ccff?style=flat-square" />
+          <img alt="Static Badge" src="https://img.shields.io/badge/%E6%97%A5%E6%9C%AC%E8%AA%9E-yellow?style=flat-square" />
+        </p>
+        <p class="bds">
+          <h4>Programming lanugages</h4>
+          <figure><embed src="https://wakatime.com/share/@018c1993-fbe1-48fd-9551-91e287631e43/64009a7d-144f-47fb-a8b0-bc68d30a656b.svg"></embed></figure>        </p>
+        <p>
+          <el-avatar :size="60" src="https://blog.1l1.icu/images/avatar.jpg"></el-avatar>
+          <img class="desc" src="https://readme-typing-svg.herokuapp.com?font=Poppins&pause=1000&color=55CAF7&background=54DCFF00&center=%E7%9C%9F&vCenter=%E7%9C%9F&repeat=%E7%9C%9F&width=435&lines=Keep+doing%2C+keep+loving%E2%9D%A4.">
+        </p>
+        <p>
+          一名来自中国大陆的高中生，现居广西壮族自治区。
+        </p>
+      </div>
+      <template #footer>
+        <span>Copyright © {{ year }} 夏沫花火zzz🌙. All right reserved.</span>
+      </template>
+    </el-card>
+  </main>
+</template>
