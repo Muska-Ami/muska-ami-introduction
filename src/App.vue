526a572015057453926fce1944c768de55f06915
夏沫花火zzz <ami@muska.zip>
2024-10-10 04:22:08 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -118,9 +118,10 @@ let year = new Date().getFullYear()
           <el-link href="https://icp.yuncheng.fun/id.php?keyword=23333333" target="_blank">团ICP备23333333号</el-link>
           </span>
         <br>
-        <span><el-link href="https://beian.miit.gov.cn" target="_blank">浙ICP备2024084850号-2</el-link></span>
-        <span><el-link href="https://beian.mps.gov.cn/#/query/webSearch?code=33038102332482"
-            target="_blank">浙公网安备33038102332482</el-link></span>
+        <span>
+          <el-link href="https://beian.miit.gov.cn" target="_blank">浙ICP备2024084850号-2</el-link>
+          |
+          <el-link href="https://beian.mps.gov.cn/#/query/webSearch?code=33038102332482" target="_blank">浙公网安备33038102332482</el-link></span>
       </template>
     </el-card>
   </main>
