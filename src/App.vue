b493620b42a2c3548b68d5eba445771650b00fd0
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-11-27 13:47:10 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -14,97 +14,114 @@ let year = new Date().getFullYear()
           <img class="desc"
             src="https://readme-typing-svg.demolab.com/?font=Poppins&pause=1000&color=55CAF7&background=54DCFF00&center=%E7%9C%9F&vCenter=%E7%9C%9F&repeat=%E7%9C%9F&width=435&lines=Keep+doing%2C+keep+loving%E2%9D%A4.">
         </p>
-        <p class="bds">
-        <h4>Languages that use</h4>
-        <img alt="Static Badge"
-          src="https://img.shields.io/badge/%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87-2fb1f3?style=flat-square" />
-        <img alt="Static Badge" src="https://img.shields.io/badge/English-2fb1f3?style=flat-square" />
-        <img alt="Static Badge"
-          src="https://img.shields.io/badge/%E6%97%A5%E6%9C%AC%E8%AA%9E-yellow?style=flat-square" />
-        </p>
-        <p class="bds">
+        <div class="bds">
+          <h4>Languages that use</h4>
+          <img alt="Static Badge"
+            src="https://img.shields.io/badge/%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87-2fb1f3?style=flat-square" />
+          <img alt="Static Badge" src="https://img.shields.io/badge/English-2fb1f3?style=flat-square" />
+          <img alt="Static Badge"
+            src="https://img.shields.io/badge/%E6%97%A5%E6%9C%AC%E8%AA%9E-yellow?style=flat-square" />
+        </div>
+        <div class="bds">
           <h4>DevOps & Stats</h4>
-          <figure><embed src="https://wakatime.com/share/@018c1993-fbe1-48fd-9551-91e287631e43/93c5700a-5367-4e24-8bed-6dcd8622d680.svg"></embed></figure>
-          <figure><embed src="https://wakatime.com/share/@018c1993-fbe1-48fd-9551-91e287631e43/e971222b-2bf7-442f-963c-7932275e3b90.svg"></embed></figure>
-        </p>
-        <p>
+          <figure><embed
+              src="https://wakatime.com/share/@018c1993-fbe1-48fd-9551-91e287631e43/93c5700a-5367-4e24-8bed-6dcd8622d680.svg"></embed>
+          </figure>
+          <figure><embed
+              src="https://wakatime.com/share/@018c1993-fbe1-48fd-9551-91e287631e43/e971222b-2bf7-442f-963c-7932275e3b90.svg"></embed>
+          </figure>
+        </div>
+        <div>
           一名来自中国大陆的高中猫猫，现居广西壮族自治区。
-        </p>
-        <p>
+          <div>
+            <span>务实</span>
+            <el-progress :percentage="65" color="#66CCFF" />
+            <span>懒惰</span>
+            <el-progress :percentage="47" color="#7C4DFF" />
+            <span>可爱</span>
+            <el-progress :percentage="83" color="#F48FB1" />
+            <span>傲娇</span>
+            <el-progress :percentage="76" color="#FFE082" />
+            <span>理智</span>
+            <el-progress :percentage="97" color="#00838F" />
+            <span>聪慧</span>
+            <el-progress :percentage="88" color="#FFAB91" />
+          </div>
           喵喵喵？我很可愛い，请给我钱喵！
           <br>才怪，本猫猫不会乱要钱哒qwq
-        </p>
-        <p>
-        <h4>Lab</h4>
-        这是花火以及几个朋友的实验室喵，欢迎造访w （目前看来已经凉了
-        <br><el-link href="https://github.com/k43Lab" target="_blank">k43Lab</el-link>
-        </p>
-        <p>
-        <h4>Network</h4>
-        欢迎来造访我的网络 Muska Network~
-        <br><el-link href="https://muska.zip" target="_blank">Muska Network</el-link>
-        </p>
-        <p class="bds">
-        <h4>What I can do & can't</h4>
-        我可以写前端！
-        <br>我也可以帮你看后端代码，但是请不要太为难我去看一些抽象的语言，暂时无法理解比如 Rust, Haskell 这样的语言哦~
-        <br>还有更多我可以帮你的......
-        <br>
-        <img src="https://img.shields.io/badge/Java-%20?style=flat-square&logo=openjdk&logoColor=white&color=a57829"
-          alt="Java">
-        <img src="https://img.shields.io/badge/Kotlin-%20?style=flat-square&logo=kotlin&logoColor=white&color=7F52FF"
-          alt="Kotlin">
-        <img src="https://img.shields.io/badge/Dart-%20?style=flat-square&logo=dart&logoColor=white&color=0175C2"
-          alt="Dart">
-        <img src="https://img.shields.io/badge/Flutter-%20?style=flat-square&logo=flutter&logoColor=white&color=02569B"
-          alt="Flutter">
-        <img src="https://img.shields.io/badge/HTML5-%20?style=flat-square&logo=html5&logoColor=white&color=E34F26"
-          alt="HTML5">
-        <img
-          src="https://img.shields.io/badge/JavaScript-%20?style=flat-square&logo=javascript&logoColor=white&color=F7DF1E"
-          alt="JavaScript">
-        <img
-          src="https://img.shields.io/badge/TypeScript-%20?style=flat-square&logo=typescript&logoColor=white&color=3178C6"
-          alt="TypeScript">
-        <img src="https://img.shields.io/badge/Vue3-%20?style=flat-square&logo=vuedotjs&logoColor=white&color=4FC08D"
-          alt="Vue3">
-        <img src="https://img.shields.io/badge/CSS3-%20?style=flat-square&logo=css3&logoColor=white&color=1572B6"
-          alt="CSS3">
-        <img
-          src="https://img.shields.io/badge/Node.js-%20?style=flat-square&logo=nodedotjs&logoColor=white&color=339933"
-          alt="Node.js">
-        <img src="https://img.shields.io/badge/Pug-%20?style=flat-square&logo=pug&logoColor=white&color=A86454"
-          alt="Pug">
-        <img src="https://img.shields.io/badge/EJS-%20?style=flat-square&logo=ejs&logoColor=white&color=B4CA65"
-          alt="EJS">
-        <img src="https://img.shields.io/badge/Bun-%20?style=flat-square&logo=bun&logoColor=white&color=000000"
-          alt="Bun">
-        <br>当然其他语言我也可以尽力阅读的呐，不过太复杂的就看不懂惹TwT
-        <br>其他方面上请<b>不要</b>叫我男娘！我非常不喜欢这个称呼qaq
-        <br>
-        </p>
-        <p class="bds">
-        <h4>Public Keys</h4>
-        <h5>GPG Public Keys</h5>
-        <el-link href="https://github.com/Muska-Ami.gpg" target="_blank">GitHub</el-link>
-        <h5>SSH Public Keys</h5>
-        <el-link href="https://github.com/Muska-Ami.keys" target="_blank">GitHub</el-link>
-        </p>
-        <p class="bds">
-        <h4>Links</h4>
-        <el-link href="https://blog.1l1.icu" target="_blank">
-          <img alt="Static Badge" src="https://img.shields.io/badge/Muska_Ami%E3%81%AEBLOG-blue?style=flat-square" />
-        </el-link>
-        <el-link href="https://github.com/Muska-Ami" target="_blank">
-          <img alt="Static Badge" src="https://img.shields.io/badge/GitHub-black?style=flat-square" />
-        </el-link>
-        <el-link href="https://x.com/Muska_Ami" target="_blank">
-          <img alt="Static Badge" src="https://img.shields.io/badge/Twitter%2FX-skyblue?style=flat-square" />
-        </el-link>
-        <el-link href="https://gravatar.com/muskaami" target="_blank">
-          <img alt="Static Badge" src="https://img.shields.io/badge/Gravatar-blue?style=flat-square" />
-        </el-link>
-        </p>
+        </div>
+        <div>
+          <h4>Lab</h4>
+          这是花火以及几个朋友的实验室喵，欢迎造访w （目前看来已经凉了
+          <br><el-link href="https://github.com/k43Lab" target="_blank">k43Lab</el-link>
+        </div>
+        <div>
+          <h4>Network</h4>
+          欢迎来造访我的网络 Muska Network~
+          <br><el-link href="https://muska.zip" target="_blank">Muska Network</el-link>
+        </div>
+        <div class="bds">
+          <h4>What I can do & can't</h4>
+          我可以写前端！
+          <br>我也可以帮你看后端代码，但是请不要太为难我去看一些抽象的语言，暂时无法理解比如 Rust, Haskell 这样的语言哦~
+          <br>还有更多我可以帮你的......
+          <br>
+          <img src="https://img.shields.io/badge/Java-%20?style=flat-square&logo=openjdk&logoColor=white&color=a57829"
+            alt="Java">
+          <img src="https://img.shields.io/badge/Kotlin-%20?style=flat-square&logo=kotlin&logoColor=white&color=7F52FF"
+            alt="Kotlin">
+          <img src="https://img.shields.io/badge/Dart-%20?style=flat-square&logo=dart&logoColor=white&color=0175C2"
+            alt="Dart">
+          <img
+            src="https://img.shields.io/badge/Flutter-%20?style=flat-square&logo=flutter&logoColor=white&color=02569B"
+            alt="Flutter">
+          <img src="https://img.shields.io/badge/HTML5-%20?style=flat-square&logo=html5&logoColor=white&color=E34F26"
+            alt="HTML5">
+          <img
+            src="https://img.shields.io/badge/JavaScript-%20?style=flat-square&logo=javascript&logoColor=white&color=F7DF1E"
+            alt="JavaScript">
+          <img
+            src="https://img.shields.io/badge/TypeScript-%20?style=flat-square&logo=typescript&logoColor=white&color=3178C6"
+            alt="TypeScript">
+          <img src="https://img.shields.io/badge/Vue3-%20?style=flat-square&logo=vuedotjs&logoColor=white&color=4FC08D"
+            alt="Vue3">
+          <img src="https://img.shields.io/badge/CSS3-%20?style=flat-square&logo=css3&logoColor=white&color=1572B6"
+            alt="CSS3">
+          <img
+            src="https://img.shields.io/badge/Node.js-%20?style=flat-square&logo=nodedotjs&logoColor=white&color=339933"
+            alt="Node.js">
+          <img src="https://img.shields.io/badge/Pug-%20?style=flat-square&logo=pug&logoColor=white&color=A86454"
+            alt="Pug">
+          <img src="https://img.shields.io/badge/EJS-%20?style=flat-square&logo=ejs&logoColor=white&color=B4CA65"
+            alt="EJS">
+          <img src="https://img.shields.io/badge/Bun-%20?style=flat-square&logo=bun&logoColor=white&color=000000"
+            alt="Bun">
+          <br>当然其他语言我也可以尽力阅读的呐，不过太复杂的就看不懂惹TwT
+          <br>其他方面上请<b>不要</b>叫我男娘！我非常不喜欢这个称呼qaq
+          <br>
+        </div>
+        <div class="bds">
+          <h4>Public Keys</h4>
+          <h5>GPG Public Keys</h5>
+          <el-link href="https://github.com/Muska-Ami.gpg" target="_blank">GitHub</el-link>
+          <h5>SSH Public Keys</h5>
+          <el-link href="https://github.com/Muska-Ami.keys" target="_blank">GitHub</el-link>
+        </div>
+        <div class="bds">
+          <h4>Links</h4>
+          <el-link href="https://blog.1l1.icu" target="_blank">
+            <img alt="Static Badge" src="https://img.shields.io/badge/Muska_Ami%E3%81%AEBLOG-blue?style=flat-square" />
+          </el-link>
+          <el-link href="https://github.com/Muska-Ami" target="_blank">
+            <img alt="Static Badge" src="https://img.shields.io/badge/GitHub-black?style=flat-square" />
+          </el-link>
+          <el-link href="https://x.com/Muska_Ami" target="_blank">
+            <img alt="Static Badge" src="https://img.shields.io/badge/Twitter%2FX-skyblue?style=flat-square" />
+          </el-link>
+          <el-link href="https://gravatar.com/muskaami" target="_blank">
+            <img alt="Static Badge" src="https://img.shields.io/badge/Gravatar-blue?style=flat-square" />
+          </el-link>
+        </div>
       </div>
       <template #footer>
         <span>Copyright © {{ year }} <a class="cr-a" href="https://1l1.icu" target="_blank">夏沫花火zzz🌙</a>.
@@ -119,7 +136,8 @@ let year = new Date().getFullYear()
         <span>
           <el-link href="https://beian.miit.gov.cn" target="_blank">浙ICP备2024084850号-2</el-link>
           |
-          <el-link href="https://beian.mps.gov.cn/#/query/webSearch?code=33038102332482" target="_blank">浙公网安备33038102332482</el-link>
+          <el-link href="https://beian.mps.gov.cn/#/query/webSearch?code=33038102332482"
+            target="_blank">浙公网安备33038102332482</el-link>
         </span>
       </template>
     </el-card>
